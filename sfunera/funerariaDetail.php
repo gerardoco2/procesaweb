063a4780d4aef575b9ce21425071a2820f6268ef
Gerardo <gerardocolina@gmail.com>
2024-03-09 20:31:21 -0400

    style  added

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -42,6 +42,23 @@ if ( !empty($ced) )
 			$benef_arr = file(trim($filas));
 			
 			echo "
+			<style>
+				table {
+				font-family: arial, sans-serif;
+				border-collapse: collapse;
+				width: 100%;
+				}
+
+				td, th {
+				border: 1px solid #dddddd;
+				text-align: left;
+				padding: 8px;
+				}
+
+				tr:nth-child(even) {
+				background-color: #dddddd;
+				}
+				</style>
 			<h1> Listado de sus Beneficiarios Registrados en el Servicio Funerario </h1>
 			<table>
 				<tr>
