545cb8b4e6ec7708673c2b28231cb144574addc1
Gerardo <gerardocolina@gmail.com>
2024-03-16 16:46:59 -0400

    removed dompdf

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -1,27 +1,9 @@
 <?php
-require 'vendor/autoload.php';
+
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 global $app, $_SERVER;
-
-
-
-// reference the Dompdf namespace
-use Dompdf\Dompdf;
-
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
-
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
-
-// Render the HTML as PDF
-$dompdf->render();
-
-// Output the generated PDF to Browser
-$dompdf->stream();
 
 
 
