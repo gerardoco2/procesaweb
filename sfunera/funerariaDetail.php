2643155c0710aeb4ec656dfc2b07f29eee1b1621
Gerardo <gerardocolina@gmail.com>
2024-03-17 13:09:47 -0400

    configuracion del generacion de carnet en pdf

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -18,13 +18,25 @@ require_once($docr . '/phps/dompdf/autoload.inc.php');
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
 
 ////////////////////////////////////
 
 
 
 
-
+/*
 if ( !empty($ced) )
 {
 	$dirw = "/home/web/jcapunefm/pdfs/";
@@ -275,5 +287,5 @@ if ( !empty($ced) )
 	}
 
 } // fin - al consultar un asociado
-
+*/
 ?>
