7433e7763686696fd13d428003099b92ff76dc59
Gerardo <gerardocolina@gmail.com>
2024-03-17 11:01:41 -0400

    configuracion del generacion de carnet en pdf

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -243,8 +243,7 @@ if ( !empty($ced) )
 			echo $carnetHtml;
 
 			function generaCarnetPdf($html) {
-				// reference the Dompdf namespace
-				use Dompdf\Dompdf;
+
 			
 				// instantiate and use the dompdf class
 				$dompdf = new Dompdf();
