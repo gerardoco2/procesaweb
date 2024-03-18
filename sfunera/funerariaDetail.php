ae6d69a1d54d40efbff44284c81d6097eea41fc1
Gerardo <gerardocolina@gmail.com>
2024-03-18 13:33:32 -0400

    configuracion del generacion de carnet en pdf

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -176,7 +176,7 @@ if ( !empty($ced) )
 				<div class="container">
 				<div class="front-side">
 				<div class="logo">
-					<img src="logocapunefm.png" alt="">
+					<img src="https://capunefm.com/images/logocapunefm.png" alt="">
 				</div>
 					<div class="body">
 					<p>CAJA DE AHORROS DEL PERSONAL DE LA <br>
