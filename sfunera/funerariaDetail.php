f97bdce3c7bd0b7670e80131d9aeb690fcd4786e
Gerardo <gerardocolina@gmail.com>
2024-03-22 01:17:34 -0400

    testing the route

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -104,7 +104,146 @@ if ( !empty($ced) )
 
 			list($tipo, $cedula_afil, $bene_afiiliado, $parentesco) = explode(";", $benef_arr[0]); 
 
-			
+			echo '
+			<style>
+			.container {
+				margin-top: 60px;
+				float: left;
+				flex-direction: row;
+			  }
+			  
+			  .front-side {
+				float: left;
+				height: 300px;
+				width: 550px;
+				border: 1px solid black;
+			  }
+			  .logo ,  .footer{
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+			  }
+
+			  .body {
+				text-align: center;
+			  }
+			  .body p {
+				font-weight: 700;
+				font-size: 12px;
+				letter-spacing: 1px
+			  }
+			  
+			  .body > .title {
+				fonr-weight: 800;
+			  }
+			  
+			  .footer img {
+				width: 200px
+			  }
+			  
+			  .affiliate-info {
+				display: flex;
+				justify-content: space-around;
+			  }
+			  
+			  .affiliate-info  p {
+				margin: 0;
+			  }
+			  
+			  .benef-container {
+				height: 16px;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				margin-left: 30px;
+				margin-right: 30px
+			  }
+			  
+			  table {
+				font-family: arial, sans-serif;
+				border-collapse: collapse;
+				width: 90%;
+				margin: 0 auto;
+			  }
+			  
+			  td, th {
+				text-align: left;
+				padding: 2px;
+				font-size: 12px
+			  }
+			</style>
+			<div>
+
+				<div class="container">
+				<div class="front-side">
+				<div class="logo">
+					<img src="https://capunefm.com/images/logocapunefm.png" alt="">
+				</div>
+					<div class="body">
+					<p>CAJA DE AHORROS DEL PERSONAL DE LA <br>
+					UNIVERSIDAD NACIONAL EXPERIMENTAL <br>
+						FRANCISCO DE MIRANDA
+					CAPUNEFM</p>
+
+					<div class="title">
+						<h3>CARNET DE AFILIACION</h3>
+					</div>
+					<div class="titular">
+						<h4>'. $bene_afiiliado  . '</h4>
+					</div>
+				<div class="footer">
+					<img src="https://www.necropolisfuneral.com/assets/img/logo_necrolpolis-footer.png" alt="">
+				</div>   
+					</div>
+
+				</div>
+
+				<div class="front-side">
+				<div class="logo">
+					<img src="https://capunefm.com/images/logocapunefm.png" alt="">
+				</div>
+				<div class="affiliate-info">
+					<div class="name">
+					<p>Nombres y Apellidos del Afiliado:</p>
+					'. $bene_afiiliado .'
+					</div>
+					<div class="id">
+					<p>Cedula:</p>
+					'. $cedula_afil .'
+					</div>
+				</div>
+					<div class="body">
+					
+
+					<div class="beneficiaries">
+						<div class="benef-title">Carga Familiar</div>
+						
+
+						<table>';
+
+						for ($x = 1; $x <= sizeof($benef_arr); $x++) {
+							list($tipo, $cedula, $beneficiario, $parentesco) = explode(";", $benef_arr[$x]);
+							echo '
+							<tr>
+								<td>'. $beneficiario .'</td>
+								<td>'. $cedula .'</td>
+								<td>'. $parentesco .'</td>
+							</tr>
+							';
+						};
+
+						echo '
+						</table>
+					</div>
+
+					</div>
+
+				</div>  
+				</div>
+
+			</div>
+			';
+
 			//echo $carnetHtml;
 
 			function generaCarnetPdf($html) {
