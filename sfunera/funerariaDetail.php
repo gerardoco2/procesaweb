373b70097177851df0eb4d161312ce11a883559d
Gerardo <gerardocolina@gmail.com>
2024-03-22 01:13:15 -0400

    testing the route

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -103,8 +103,6 @@ if ( !empty($ced) )
 			echo '<h3 style="margin: 20px; ">Descarga tu Carnet del servicio funerario aqui </h3> ';
 
 			list($tipo, $cedula_afil, $bene_afiiliado, $parentesco) = explode(";", $benef_arr[0]); 
-			echo  '<img src="/logocapunefm.png"'.'>' ;
-			echo '<img src="' . JURI::base() . 'images/stories/capunefm/iconcapunefm.png />';
 
 			$carnetHtml = '
 			<style>
