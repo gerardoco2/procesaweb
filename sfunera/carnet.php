3210ed1b354a838bf3da39f50e0f150cf4e11d89
Gerardo <gerardocolina@gmail.com>
2024-03-26 04:18:00 -0400

    changed flexbox to use plain css

--- sfunera/carnet.php
+++ sfunera/carnet.php
@@ -1,150 +1,162 @@
         <style>
-			.container {
-				margin-top: 60px;
-				float: left;
-				flex-direction: row;
-			  }
-			  
-			  .front-side {
-				float: left;
-				height: 300px;
-				width: 550px;
-				border: 1px solid black;
-			  }
-			  .logo ,  .footer{
-				display: flex;
-				flex-direction: row;
-				justify-content: center;
-			  }
+            /* Container */
+            .container {
+            margin-top: 60px;
+            display: inline-block; /* Display inline-block to allow elements next to it */
+            width: 550px; /* Set container width */
+            }
 
-			  .body {
-				text-align: center;
-			  }
-			  .body p {
-				font-weight: 700;
-				font-size: 12px;
-				letter-spacing: 1px
-			  }
-			  
-			  .body > .title {
-				font-weight: 800;
-			  }
-			  
-			  .footer img {
-				width: 200px
-			  }
-			  
-			  .affiliate-info {
-				display: flex;
-				justify-content: space-around;
-			  }
-			  
-			  .affiliate-info  p {
-				margin: 0;
-			  }
-			  
-			  .benef-container {
-				height: 16px;
-				display: flex;
-				flex-direction: row;
-				justify-content: space-between;
-				margin-left: 30px;
-				margin-right: 30px
-			  }
-			  
-			  table {
-				font-family: arial, sans-serif;
-				border-collapse: collapse;
-				width: 90%;
-				margin: 0 auto;
-			  }
-			  
-			  td, th {
-				text-align: left;
-				padding: 2px;
-				font-size: 12px
-			  }
+            /* Front side */
+            .front-side {
+            float: left;
+            height: 300px;
+            width: 100%; /* Set width to 100% to fill container */
+            border: 1px solid black;
+            }
+
+            /* Logo and footer */
+            .logo,
+            .footer {
+            text-align: center; /* Center content horizontally */
+            padding-top: 10px; /* Add top padding */
+            }
+
+            .footer img {
+            width: 200px;
+            }
+
+            /* Body */
+            .body {
+            text-align: center;
+            }
+
+            .body p {
+            font-weight: 700;
+            font-size: 12px;
+            letter-spacing: 1px;
+            }
+
+            .body > .title {
+            font-weight: 800;
+            }
+
+            /* Affiliate information */
+            .affiliate-info {
+            display: table; /* Display as a table for even spacing */
+            width: 100%; /* Span full width */
+            margin: 0 auto; /* Center horizontally */
+            }
+
+            .affiliate-info p {
+            margin: 0;
+            display: table-cell; /* Display as table cells */
+            vertical-align: middle; /* Vertically align content in the middle */
+            }
+
+            /* Benefits container */
+            .benef-container {
+            height: 16px;
+            display: table; /* Display as a table for even spacing */
+            width: 100%; /* Span full width */
+            margin: 0 auto; /* Center horizontally */
+            }
+
+            .benef-container > p {
+            display: table-cell; /* Display as table cells */
+            vertical-align: middle; /* Vertically align content in the middle */
+            }
+
+            /* Table styles */
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%; /* Set table width to 100% of its container */
+            margin: 0 auto; /* Center horizontally */
+            }
+
+            td, th {
+            text-align: left;
+            padding: 2px;
+            font-size: 12px;
+            }
 		</style>
 
 			<div>
 
 				<div class="container">
-				<div class="front-side">
-				<div class="logo">
-					<img src="https://capunefm.com/images/logocapunefm.png" alt="">
-				</div>
-					<div class="body">
-					<p>CAJA DE AHORROS DEL PERSONAL DE LA <br>
-					UNIVERSIDAD NACIONAL EXPERIMENTAL <br>
-						FRANCISCO DE MIRANDA
-					CAPUNEFM</p>
+                    <div class="front-side">
+                        <div class="logo">
+                            <img src="https://capunefm.com/images/logocapunefm.png" alt="">
+                        </div>
+                        <div class="body">
+                            <p>CAJA DE AHORROS DEL PERSONAL DE LA <br>
+                            UNIVERSIDAD NACIONAL EXPERIMENTAL <br>
+                            FRANCISCO DE MIRANDA CAPUNEFM</p>
 
-					<div class="title">
-						<h3>CARNET DE AFILIACION</h3>
-					</div>
-					<div class="titular">
-						<h4>'. $bene_afiiliado  . '</h4>
-					</div>
-				<div class="footer">
-					<img src="https://www.necropolisfuneral.com/assets/img/logo_necrolpolis-footer.png" alt="">
-				</div>   
-					</div>
+                            <div class="title">
+                                <h3>CARNET DE AFILIACION</h3>
+                            </div>
+                            <div class="titular">
+                                <h4>'. $bene_afiiliado  . '</h4>
+                            </div>
+                            <div class="footer">
+                                <img src="https://www.necropolisfuneral.com/assets/img/logo_necrolpolis-footer.png" alt="">
+                            </div>   
+                        </div>
 
-				</div>
+                    </div>
 
-				<div class="front-side">
-				<div class="logo">
-					<img src="https://capunefm.com/images/logocapunefm.png" alt="">
-				</div>
-				<div class="affiliate-info">
-					<div class="name">
-					<p>Nombres y Apellidos del Afiliado:</p>
-					Gerardo Colina
-					</div>
-					<div class="id">
-					<p>Cedula:</p>
-					20569539
-					</div>
-				</div>
-					<div class="body">
-					
+                    <div class="front-side">
+                        <div class="logo">
+                            <img src="https://capunefm.com/images/logocapunefm.png" alt="">
+                        </div>
+                        <div class="affiliate-info">
+                            <div class="name">
+                            <p>Nombres y Apellidos del Afiliado:</p>
+                            Gerardo Colina
+                            </div>
+                            <div class="id">
+                            <p>Cedula:</p>
+                            20569539
+                            </div>
+                        </div>
+                        <div class="body">
+                        
 
-					<div class="beneficiaries">
-						<div class="benef-title">Carga Familiar</div>
-						
+                            <div class="beneficiaries">
+                                <div class="benef-title">Carga Familiar</div>
+                                <table>
+                                    <tr>
+                                        <td>Nelis Quintero</td>
+                                        <td>7484589</td>
+                                        <td>Madre</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Juan Colina</td>
+                                        <td>9503183</td>
+                                        <td>Padre</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Juan Colina</td>
+                                        <td>9503183</td>
+                                        <td>Padre</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Juan Colina</td>
+                                        <td>9503183</td>
+                                        <td>Padre</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Juan Colina</td>
+                                        <td>9503183</td>
+                                        <td>Padre</td>
+                                    </tr>
+                                </table>
+                            </div>
 
-						<table>
-							<tr>
-								<td>Nelis Quintero</td>
-								<td>7484589</td>
-								<td>Madre</td>
-							</tr>
-                            <tr>
-								<td>Juan Colina</td>
-								<td>9503183</td>
-								<td>Padre</td>
-							</tr>
-                            <tr>
-								<td>Juan Colina</td>
-								<td>9503183</td>
-								<td>Padre</td>
-							</tr>
-                            <tr>
-								<td>Juan Colina</td>
-								<td>9503183</td>
-								<td>Padre</td>
-							</tr>
-                            <tr>
-								<td>Juan Colina</td>
-								<td>9503183</td>
-								<td>Padre</td>
-							</tr>
-						</table>
-					</div>
+                        </div>
 
-					</div>
-
-				</div>  
+                    </div>  
 				</div>
 
 			</div>
