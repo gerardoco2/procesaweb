517c990e66b4c41b0b44b06df4e63f0aaf56b3e5
Gerardo <gerardocolina@gmail.com>
2024-03-25 23:36:32 -0400

    testing pdf

--- sfunera/funerariaDetail.php
+++ sfunera/funerariaDetail.php
@@ -253,7 +253,158 @@ if ( !empty($ced) )
 
 				// instantiate and use the dompdf class
 				$dompdf = new Dompdf($options);
-				$dompdf->loadHtml($carnetHtml);
+				$dompdf->loadHtml('
+				<style>
+				.container {
+					margin-top: 60px;
+					float: left;
+					flex-direction: row;
+				  }
+				  
+				  .front-side {
+					float: left;
+					height: 300px;
+					width: 550px;
+					border: 1px solid black;
+				  }
+				  .logo ,  .footer{
+					display: flex;
+					flex-direction: row;
+					justify-content: center;
+				  }
+	
+				  .body {
+					text-align: center;
+				  }
+				  .body p {
+					font-weight: 700;
+					font-size: 12px;
+					letter-spacing: 1px
+				  }
+				  
+				  .body > .title {
+					font-weight: 800;
+				  }
+				  
+				  .footer img {
+					width: 200px
+				  }
+				  
+				  .affiliate-info {
+					display: flex;
+					justify-content: space-around;
+				  }
+				  
+				  .affiliate-info  p {
+					margin: 0;
+				  }
+				  
+				  .benef-container {
+					height: 16px;
+					display: flex;
+					flex-direction: row;
+					justify-content: space-between;
+					margin-left: 30px;
+					margin-right: 30px
+				  }
+				  
+				  table {
+					font-family: arial, sans-serif;
+					border-collapse: collapse;
+					width: 90%;
+					margin: 0 auto;
+				  }
+				  
+				  td, th {
+					text-align: left;
+					padding: 2px;
+					font-size: 12px
+				  }
+			</style>
+	
+				<div>
+	
+					<div class="container">
+					<div class="front-side">
+					<div class="logo">
+						<img src="https://capunefm.com/images/logocapunefm.png" alt="">
+					</div>
+						<div class="body">
+						<p>CAJA DE AHORROS DEL PERSONAL DE LA <br>
+						UNIVERSIDAD NACIONAL EXPERIMENTAL <br>
+							FRANCISCO DE MIRANDA
+						CAPUNEFM</p>
+	
+						<div class="title">
+							<h3>CARNET DE AFILIACION</h3>
+						</div>
+						<div class="titular">
+							<h4>'. $bene_afiiliado  . '</h4>
+						</div>
+					<div class="footer">
+						<img src="https://www.necropolisfuneral.com/assets/img/logo_necrolpolis-footer.png" alt="">
+					</div>   
+						</div>
+	
+					</div>
+	
+					<div class="front-side">
+					<div class="logo">
+						<img src="https://capunefm.com/images/logocapunefm.png" alt="">
+					</div>
+					<div class="affiliate-info">
+						<div class="name">
+						<p>Nombres y Apellidos del Afiliado:</p>
+						Gerardo Colina
+						</div>
+						<div class="id">
+						<p>Cedula:</p>
+						20569539
+						</div>
+					</div>
+						<div class="body">
+						
+	
+						<div class="beneficiaries">
+							<div class="benef-title">Carga Familiar</div>
+							
+	
+							<table>
+								<tr>
+									<td>Nelis Quintero</td>
+									<td>7484589</td>
+									<td>Madre</td>
+								</tr>
+								<tr>
+									<td>Juan Colina</td>
+									<td>9503183</td>
+									<td>Padre</td>
+								</tr>
+								<tr>
+									<td>Juan Colina</td>
+									<td>9503183</td>
+									<td>Padre</td>
+								</tr>
+								<tr>
+									<td>Juan Colina</td>
+									<td>9503183</td>
+									<td>Padre</td>
+								</tr>
+								<tr>
+									<td>Juan Colina</td>
+									<td>9503183</td>
+									<td>Padre</td>
+								</tr>
+							</table>
+						</div>
+	
+						</div>
+	
+					</div>  
+					</div>
+	
+				</div>
+				');
 
 				$stream = true;
 				// (Optional) Setup the paper size and orientation
@@ -277,7 +428,7 @@ if ( !empty($ced) )
 
 			}
 			
-			//generaCarnetPdf($carnetHtml);
+				generaCarnetPdf($carnetHtml);
 
                 remover_antiguos($dirw);
 	}
