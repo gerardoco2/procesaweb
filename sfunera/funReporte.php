d128e69972338c563d0bee9c62d6394ab78678af
Gerardo <gerardocolina@gmail.com>
2024-03-28 10:23:21 -0400

    added reporte

--- sfunera/funReporte.php
+++ sfunera/funReporte.php
@@ -42,11 +42,13 @@ if ( $tip === "Registered" || $tip === "Guest" )
 
 	$ejec = exec($raiz . $rdir . "ejec_pvx_sfunera_reporte 2>&1");
 
-    $file = $raiz . $rdir . "REPORTE_FUNERA_AFIBENE.PDF";
+    $file = $raiz . $rdir . "REPORTE_FUNERA_AFIBENE.pdf";
 
 	if ( file_exists($file) )
 	{
-	
+		copy($file, $dirw . $hashced . ".pdf");
+                $updf = JURI::base() . "pdfs/" . $hashced . ".pdf";
+                unlink( $filas );
                // $updf = JURI::base() . "pdfs/" . $hashced . ".pdf";
                 //$updf = JURI::base() . "pdfs/" . $hashced . ".pdf";
                 //unlink( $filas );
