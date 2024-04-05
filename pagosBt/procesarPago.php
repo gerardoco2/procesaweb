38751767d63fa92c3bd2b570e7f0c985e684fd5f
Gerardo <gerardocolina@gmail.com>
2024-04-05 00:10:51 -0400

    removed  colom by mistake

--- pagosBt/procesarPago.php
+++ pagosBt/procesarPago.php
@@ -4,11 +4,10 @@ $cedula = isset($_POST['cedula']) ? $_POST['cedula'] : null;
 $telefono = isset($_POST['telefono']) ? $_POST['telefono'] : null;
 $banco = isset($_POST['banco']) ? $_POST['banco'] : null;
 $token = isset($_POST['token']) ? $_POST['token'] : null;
-
-
+/*
 if (empty($cedula) || empty($telefono) || empty($banco) || empty($token) ) {
   die('Por favor ingresa todos los datos');
-}
+}*/
 
 
 $apiUrl = 'http://190.202.9.207:8080/RestTesoro_C2P/com/services/botonDePago/pago'; 
