1a87763700c805be81245359f89a15f7cfa2954f
Gerardo <gerardocolina@gmail.com>
2024-04-13 11:47:07 -0400

    pago result

---
+++ pagosBt/RESPALDO.php
@@ -0,0 +1,73 @@
+<?php
+
+$cedula = isset($_POST['cedula']) ? $_POST['cedula'] : null;
+$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : null;
+$banco = isset($_POST['banco']) ? $_POST['banco'] : null;
+$token = isset($_POST['token']) ? $_POST['token'] : null;
+/*
+if (empty($cedula) || empty($telefono) || empty($banco) || empty($token) ) {
+  die('Por favor ingresa todos los datos');
+}*/
+
+
+$apiUrl = 'http://190.202.9.207:8080/RestTesoro_C2P/com/services/bancos'; 
+$postData = [
+  "canal"=>"06",
+  "celular"=>"04241234128",
+  "banco"=>"0128",
+  "RIF"=>"J301578970",
+  "cedula"=>"V1234567",
+  "monto"=>"5000.00",
+  "token"=>"20191231",
+  "concepto"=> " paga",
+  "codAfiliado"=>"104663",
+  "comercio"=>""
+];
+
+$ch = curl_init($apiUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+
+if (curl_errno($ch)) {
+  die('API Error: ' . curl_error($ch));
+}
+
+
+$data = json_decode($response, true);
+
+foreach ($data as $key => $value) {
+  echo "Key: $key; Value: $value\n";
+}
+
+if (isset($data['error'])) {
+  $result = 'API Error: ' . $data['error'];
+} else {
+  $status = $data['status'] ? 'Active' : 'Inactive';
+  $result = "Respuesta del banco: {$data['codres']}, resultado: {$data['descRes']}, Referencia: {$data['fecha']},
+   Monto operacion {$data['monto']} ";
+}
+
+
+
+echo <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Resultado del pago</title>
+</head>
+<body>
+  <div id="result">
+    $result
+  </div>
+</body>
+</html>
+HTML;
+
+?>
+
+
--- pagosBt/procesarPago.php
+++ pagosBt/procesarPago.php
@@ -1,71 +1,116 @@
-<?php
-
-$cedula = isset($_POST['cedula']) ? $_POST['cedula'] : null;
-$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : null;
-$banco = isset($_POST['banco']) ? $_POST['banco'] : null;
-$token = isset($_POST['token']) ? $_POST['token'] : null;
-/*
-if (empty($cedula) || empty($telefono) || empty($banco) || empty($token) ) {
-  die('Por favor ingresa todos los datos');
-}*/
 
 
-$apiUrl = 'http://190.202.9.207:8080/RestTesoro_C2P/com/services/bancos'; 
-$postData = [
-  "canal"=>"06",
-  "celular"=>"04241234128",
-  "banco"=>"0128",
-  "RIF"=>"J301578970",
-  "cedula"=>"V1234567",
-  "monto"=>"5000.00",
-  "token"=>"20191231",
-  "concepto"=> " paga",
-  "codAfiliado"=>"104663",
-  "comercio"=>""
-];
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html style="-moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; background-color: #464646; margin: 0; padding: 0;">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="format-detection" content="telephone=no">
+        <title>Pago exitoso </title>
+        
+    </head>
+    <body background="#d7d7d7" class="generic-template" style="-moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; background-color: #d7d7d7; margin: 0; padding: 0;">
+        <!-- Header Start -->
+        <div class="bg-white header" bgcolor="#ffffff" style="background-color: white; width: 100%;">
+            <table align="center" bgcolor="#ffffff" style="border-left: 10px solid white; border-right: 10px solid white; max-width: 600px; width: 100%;">
+                <tr height="80">
+                    <td align="left" class="vertical-align-middle" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: middle;">
+                      
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <!-- Header End -->
 
-$ch = curl_init($apiUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        <!-- Content Start -->
+        <table cellpadding="0" cellspacing="0" cols="1" bgcolor="#d7d7d7" align="center" style="max-width: 600px;">
+            <tr bgcolor="#d7d7d7">
+                <td height="50" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+            </tr>
 
-$response = curl_exec($ch);
-curl_close($ch);
+            <!-- This encapsulation is required to ensure correct rendering on Windows 10 Mail app. -->
+            <tr bgcolor="#d7d7d7">
+                <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                    <!-- Seperator Start -->
+                    <table cellpadding="0" cellspacing="0" cols="1" bgcolor="#d7d7d7" align="center" style="max-width: 600px; width: 100%;">
+                        <tr bgcolor="#d7d7d7">
+                            <td height="30" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                    </table>
+                    <!-- Seperator End -->
 
+ <!-- Generic Pod Left Aligned with Price breakdown Start -->
+                    <table align="center" cellpadding="0" cellspacing="0" cols="3" bgcolor="white" class="bordered-left-right" style="border-left: 10px solid #d7d7d7; border-right: 10px solid #d7d7d7; max-width: 600px; width: 100%;">
+                        <tr height="50"><td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td></tr>
+                        <tr align="center">
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td class="text-primary" style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                <img src="http://dgtlmrktng.s3.amazonaws.com/go/emails/generic-email-template/tick.png" alt="GO" width="50" style="border: 0; font-size: 0; margin: 0; max-width: 100%; padding: 0;">
+                            </td>
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                        <tr height="17"><td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td></tr>
+                        <tr align="center">
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td class="text-primary" style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                <h1 style="color: #F16522; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 30px; font-weight: 700; line-height: 34px; margin-bottom: 0; margin-top: 0;"> Pago Recibido</h1>
+                            </td>
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                        <tr height="30"><td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td></tr>
+                        <tr align="left">
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                <p style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">
+                                    Hola Gerardo , 
+                                </p>
+                            </td>
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                        <tr height="10"><td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td></tr>
+                        <tr align="left">
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                <p style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;">Hemos Recibido el pago de su cuota!</p>
+                                <br>
+                                <p style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0; "><strong>Detalles del pago:</strong><br/>
 
-if (curl_errno($ch)) {
-  die('API Error: ' . curl_error($ch));
-}
+Monto: 95 Bs <br/>
+Detalle: Servicio Funerario Mes Abril.<br/></p>
+                                    <br>
+                               
+                            </td>
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                        <tr height="30">
+                            <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td style="border-bottom: 1px solid #D3D1D1; color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
+                        <tr height="30"><td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td></tr>
+                        <tr align="center">
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                            <td style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;">
+                                <p style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;"><strong> Referencia: 783492</strong></p>
+             
+                                <p style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 22px; margin: 0;"></p>
+                            </td>
+                            <td width="36" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
 
+                        <tr height="50">
+                            <td colspan="3" style="color: #464646; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 16px; vertical-align: top;"></td>
+                        </tr>
 
-$data = json_decode($response, true);
+                    </table>
+                    <!-- Generic Pod Left Aligned with Price breakdown End -->
 
-foreach ($data as $key => $value) {
-  echo "Key: $key; Value: $value\n";
-}
+                   
+                </td>
+            </tr>
+        </table>
+        <!-- Content End -->
 
-if (isset($data['error'])) {
-  $result = 'API Error: ' . $data['error'];
-} else {
-  $status = $data['status'] ? 'Active' : 'Inactive';
-  $result = "Respuesta del banco: {$data['codres']}, resultado: {$data['descRes']}, Referencia: {$data['fecha']},
-   Monto operacion {$data['monto']} ";
-}
-
-
-
-echo <<<HTML
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Resultado del pago</title>
-</head>
-<body>
-  <div id="result">
-    $result
-  </div>
-</body>
+        
+    </body>
 </html>
-HTML;
-
-?>
