8e8216ea7172900e53365d688e3e9151952ea077
Gerardo <gerardocolina@gmail.com>
2024-06-12 08:06:03 -0400

    testing procesar pago php

--- pagosBt/pagoExitoso.php
+++ pagosBt/pagoExitoso.php
@@ -10,21 +10,21 @@
     $rdir = "/";
 
     //del archivo de rechazoos obtener un arreglo con las lineas
-    $filas = $raiz . $rdir . $ced . "_RECHAZOS.TXT";
-    $arreglo_lineas = file($filas);
+    //---$filas = $raiz . $rdir . $ced . "_RECHAZOS.TXT";
+    /// ---$arreglo_lineas = file($filas);
 
     // obtener la linea en la posicion especificada
-    $linea_a_pagar = $arreglo_lineas[$num_linea];
+    //---$linea_a_pagar = $arreglo_lineas[$num_linea];
 
    // escribir un archivo ced_cuota_a_pagar.txt
-   $file_cuota = $raiz . $rdir . "CUOTA_A_PAGAR.TXT";
+   // ---$file_cuota = $raiz . $rdir . "CUOTA_A_PAGAR.TXT";
 
-	if ( file_exists($file_cuota) ){
-		unlink( $file_cuota );
-	}
-	touch ($file_cuota);
+	// --- if ( file_exists($file_cuota) ){
+	// ---	unlink( $file_cuota );
+	// ---}
+	// ---touch ($file_cuota);
 
-	escribir_archivo($file_cuota, $linea_a_pagar . '"\n'. $refenecia .'"'); // guardar linea con datos de cuota para su lectura por procesa
+	// ---escribir_archivo($file_cuota, $linea_a_pagar . '"\n'. $refenecia .'"'); // guardar linea con datos de cuota para su lectura por procesa
    
    //ejecutar script de procesa que hace el asiento
 
