6a07b61c7cee3a9de25710533e0487748823c703
Gerardo <gerardocolina@gmail.com>
2025-01-24 00:38:49 -0400

    REMOVED TABLE FROM THE PREVIOUS TESTING PLACE

--- scuenta/estadoc.php
+++ scuenta/estadoc.php
@@ -129,7 +129,9 @@ Este navegador no soporta lector de PDF. Por favor descargue el estado de cuenta
 
                     </table>
 					
-					<blockquote>POR FAVOR DIRIJASE A LA
+					<blockquote>
+						NOTA: 
+						POR FAVOR DIRIJASE A LA
 						OPCION DE PAGO DE CUOTAS RECHAZADAS ONLINE PARA SU CANCELACION y ACTUALIZACION DE SU ESTADO DE CUENTA, PARA MAYOR
 						INFORMACION, FAVOR COMUNICARSE A TRAVES DE NUESTROS CANALES OFICIALES:
 						infocapunefm@gmail.com ó POR WHATSAPP 0424-6071800</blockquote>
