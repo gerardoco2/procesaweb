48bca0baced07cb9fe42cac9e55e957640a292dd
Gerardo <gerardocolina@gmail.com>
2025-01-24 00:34:00 -0400

    agrega para sugerir pago de cuotas rechazadas

--- scuenta/estadoc.php
+++ scuenta/estadoc.php
@@ -128,6 +128,9 @@ Este navegador no soporta lector de PDF. Por favor descargue el estado de cuenta
                         </tbody>
 
                     </table>
-					
+					<p>POR FAVOR DIRIJASE A LA
+						OPCION DE PAGO DE CUOTAS RECHAZADAS ONLINE PARA SU CANCELACION y ACTUALIZACION DE SU ESTADO DE CUENTA, PARA MAYOR
+						INFORMACION, FAVOR COMUNICARSE A TRAVES DE NUESTROS CANALES OFICIALES:
+						infocapunefm@gmail.com ó POR WHATSAPP 0424-6071800</p>
                 <?php }?>
                     
